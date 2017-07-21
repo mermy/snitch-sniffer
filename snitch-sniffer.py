1d3a2f5e7d34b6c5f87a0d02f272384e70128677
mermy <mbroadbent@linz.govt.nz>
2017-07-21 23:38:07 +1200

    add additional code

--- snitch-sniffer.py
+++ snitch-sniffer.py
@@ -4,4 +4,7 @@ print 'hello world'
 
 print 'miriam is the best in the whole wide world'
 
-
+with open("miriam.csv", 'rb') as inputfile1:
+    in1=csv.reader(inputfile1)
+    for line in in1:
+        miriam=jane
